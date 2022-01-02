302af614830eec8f12073866098a66c42d520fe0
hunal <hsynemr59@gmail.com>
2022-01-02 19:17:44 +0300

    message

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -50,6 +51,23 @@ public class ChercherTest {
         // if you don't wait you will get NoAlertPresentException: no such alert
         Alert alert = driver.switchTo().alert();
         alert.accept();
+    }
+
+    @Test
+    public void disableButtonTest(){
+
+        WebElement button = driver.findElement(By.id("disable"));
+        System.out.println("button.isEnabled() = " + button.isEnabled()); // false
+
+        WebElement buttonInitiator = driver.findElement(By.id("enable-button"));
+        buttonInitiator.click();
+
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        System.out.println("button.isEnabled() = " + button.isEnabled()); // true
+        Assert.assertTrue(button.isEnabled(),"Verify the Button is enabled");
+
     }
 
 }
